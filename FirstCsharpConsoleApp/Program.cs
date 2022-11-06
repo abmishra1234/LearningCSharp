fdf8e1a8a39bebb8bf5ba41f5291beafcdd41be2
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-11-06 08:30:41 +0530

    First Console CSharp App

---
+++ FirstCsharpConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Solution
+    {
+
+        static void Main(string []args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("What is your name?");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " Welcome to your first exercise!!");
+        }
+    }
+}
