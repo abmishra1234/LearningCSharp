c1ede1d813a3110409d1f50ba5ff8956fa8f2d99
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-11-06 20:43:57 +0530

    Updated code for operator  ??  and  ??=

--- FirstCsharpConsoleApp/TestHelloWorld.cs
+++ FirstCsharpConsoleApp/TestHelloWorld.cs
@@ -14,17 +14,19 @@ namespace FirstCsharpConsoleApp
     {
         static void Main(string[]args)
         {
-            Console.WriteLine("Welcome to the C# basic language learning!!!");
+            //Console.WriteLine("Welcome to the C# basic language learning!!!");
 
-            Solution sln = new Solution();
-            sln.Enquiry();
+            //Solution sln = new Solution();
+            //sln.Enquiry();
 
             VariableDemoClass vdc = new VariableDemoClass(5, 23456.456, 
                 "Abinash Mishra", "9971003216", 8534369, "Noida 201301");
 
-            Console.WriteLine(vdc.getExpYear());
-            Console.WriteLine();
-            Console.WriteLine(vdc.getExpYear());
+            //Console.WriteLine(vdc.getExpYear());
+            //Console.WriteLine();
+            //Console.WriteLine(vdc.getExpYear());
+
+            vdc.testMethod();
 
 
         }
--- FirstCsharpConsoleApp/VariableDemoClass.cs
+++ FirstCsharpConsoleApp/VariableDemoClass.cs
@@ -28,10 +28,46 @@ namespace FirstCsharpConsoleApp
 
         }
 
+        public void testMethod()
+        {
+
+            object obj = null;
+            Console.WriteLine(obj);
+
+            // Demo for implicit conversion
+            int i = 25;
+            long bignum = i;
+            Console.WriteLine(bignum);
+
+            // Demo for Explicit conversion
+            float itof = i;
+            Console.WriteLine(itof);
+
+            // Learn two operator here which is not incase of C++ / Java
+//            string str = null;
+            string str = "Hello Abinash!!!";
+
+            Console.WriteLine(str ?? "Unknown String");
+
+            str ??= "New String";
+
+            Console.WriteLine(str);
+
+
+
+
+
+
+        }
+
+
+
         public int getExpYear()
         {
             return mExpYear;
         }
+
+        public 
 
 
         // How do we get all these attribute outside this class?
