fa8f97bf424984f67c4b7613bec4bca06f0f88fd
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-11-12 14:19:03 +0530

    List with Examples

---
+++ Collection_Learning_Csharp/Collection01.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Learning_Csharp
+{
+    internal class Collection01
+    {
+        List<string> l;
+
+        public Collection01()
+        {
+            // create the new list
+            l = new List<string>(10);    
+
+
+        }
+
+        protected void Print_List()
+        {
+            foreach(var item in l)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        
+        public void Solve_List()
+        {
+            string[] comedians = { "Anupam kher", "Akhay Kumar", "John Lever", "Junior mahmood", "Sharukh Khan" };
+            l.AddRange(comedians);
+
+            // Add individual item
+            l.Add("Abinash Mishra");
+            l.Add("Smita Mishra");
+
+            // Examine the count and capaity.
+            Console.WriteLine("Count and Capacity : {0}, {1}", l.Count, l.Capacity);
+
+            Print_List();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Collection_Learning_Csharp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Collection_Learning_Csharp;
+
+
+Console.WriteLine("Hello, World!");
+
+
+Collection01 cl = new Collection01();
+cl.Solve_List();
