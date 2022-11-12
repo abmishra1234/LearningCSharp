f9392e8ad6476521786966a47521991eec030a4e
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-11-12 18:20:44 +0530

    List Collection Example Completed

--- Collection_Learning_Csharp/Collection01.cs
+++ Collection_Learning_Csharp/Collection01.cs
@@ -37,6 +37,7 @@ namespace Collection_Learning_Csharp
             // Add individual item
             l.Add("Abinash Mishra");
             l.Add("Smita Mishra");
+            l.Add("Aahana Mishra");
 
             // Examine the count and capaity.
             //Console.WriteLine("Count and Capacity : {0}, {1}", l.Count, l.Capacity);
@@ -50,19 +51,54 @@ namespace Collection_Learning_Csharp
             //Print_List();
 
             l.Sort();
-            Print_List();
+            //Print_List();
 
             // Serching the List Content
             int ansInd = l.BinarySearch("Smita Mishra");
-            Console.WriteLine(ansInd);
-            Console.WriteLine(l[ansInd]);
+            //Console.WriteLine(ansInd);
+            //Console.WriteLine(l[ansInd]);
 
             // TODO: Determine if a list contains a given item
             Console.WriteLine(l.Contains("Abinash Mishra"));
 
-            l.RemoveAt(0);
-            Console.WriteLine(l.Contains("Abinash Mishra"));
-            Print_List();
+            // The Exists function gives us a more Customizable way to search
+            // by using a predicate function to examine each element in the list
+            bool found = l.Exists(x => x.Length > 12);
+            Console.WriteLine("Found : {0}", found);
+
+            // Use the Exist function to search through items
+            found = l.Exists(x => x.Contains("Abhinash Mishra"));
+            Console.WriteLine("Found : {0}", found); // False for "Abhinash Mishra"
+
+            // Use the Find method for searching through the items
+            var item = l.Find(x => x.Contains("Aahana Mishra"));
+            Console.WriteLine(item);
+
+            item = l.Find(x => x.StartsWith("A"));
+            Console.WriteLine(item);
+
+            Console.WriteLine("====================");
+
+            // Find the all item from the list which Starts With 'A'
+            var itemList = l.FindAll(x => x.StartsWith("A"));
+            foreach (var str in itemList)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("====================");
+
+            var nameList = l.FindAll(x => x.Length > 12);
+            foreach (var str in nameList)
+            {
+                Console.WriteLine(str);
+            }
+
+
+
+            //l.RemoveAt(0);
+            //Console.WriteLine(l.Contains("Abinash Mishra"));
+            //            Print_List();
 
 
         }
