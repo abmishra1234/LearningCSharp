ae11584a2ab5feffd7ef417f1f7d5115972ceae4
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-11-13 17:21:53 +0530

    String Validator Sample completed

--- CSharp_Algorithm/Program.cs
+++ CSharp_Algorithm/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using CSharp_Algorithm;
+
+
 Console.WriteLine("Hello, World!");
+
+StringAlgo sa = new StringAlgo();
+//Console.WriteLine(sa.IsUpperCase("ABCDghIJ"));
+//Console.WriteLine(sa.IsUpperCase("ABCDIJ"));
+
+//Console.WriteLine(sa.IsLowerCase("abcdefAghij"));
+//Console.WriteLine(sa.IsLowerCase("abcdefaghij"));
+
+Console.WriteLine(sa.IsPasswordComplex("abcd"));
+Console.WriteLine(sa.IsPasswordComplex("Abcd"));
+Console.WriteLine(sa.IsPasswordComplex("AbcdF"));
+Console.WriteLine(sa.IsPasswordComplex("Abcd0"));
--- CSharp_Algorithm/StringAlgo.cs
+++ CSharp_Algorithm/StringAlgo.cs
@@ -8,7 +8,24 @@ namespace CSharp_Algorithm
 {
     internal class StringAlgo
     {
+        // verification code for all Upper ?
+        public Boolean IsUpperCase(string s)
+        {
+            return s.All(char.IsUpper);
+        }
+        
+        // Verification code for all lower?
+        public Boolean IsLowerCase(string s)
+        {
+            return s.All(char.IsLower);
+        }
 
+        // verification code for password
+        public Boolean IsPasswordComplex(string s)
+        {
+            return s.Any(char.IsLower) && s.Any(char.IsUpper)
+                && s.Any(char.IsNumber);
+        }
 
 
 
