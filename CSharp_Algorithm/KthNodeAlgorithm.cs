f4631d87dab3d6aa0dd3db3fbffe5952eb9aff23
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-03-16 20:17:36 +0530

    Changes
    Changes

---
+++ CSharp_Algorithm/KthNodeAlgorithm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+// Challenge
+/*
+    Create an algorithm that deletes the Kth node from the end of the list. 
+    
+    23 -> 2 -> 34 -> 45 ->56 ->NULL
+
+    Delete K = 1
+    23 -> 2 -> 34 -> 45 ->NULL
+    
+    Delete K = 4
+    2 -> 34 -> 45 ->NULL
+
+    Approach 01: Recursive Approach
+    Solution -> Design your Algorithm First
+
+    Let's iterate the LinkedList using next pointer
+    
+    once reach the end , start unwinding or unfolding the recursion and by that time you just try
+    find the element from the end with the counter decrease by 1 with all Unwinding step,
+    Until you will reach at the K == 0.
+
+    And you also have to maintain the Curr and Prev pointer for your stack
+    
+    Approach 02 : Iterative Approach
+    Solution -> Design your Algorithm First
+
+
+
+ */
+namespace CSharp_Algorithm
+{
+    internal class CustomLinkedList
+    {
+        
+
+
+
+
+
+
+    }
+}
--- CSharp_Algorithm/Program.cs
+++ CSharp_Algorithm/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CSharp_Algorithm;
+using System;
+using System.Threading.Tasks;
 
 
 Console.WriteLine("Hello, World!");
@@ -25,6 +27,37 @@ StringAlgo sa = new StringAlgo();
 //Console.WriteLine(sa.ReverseEachWord("Hi, I am Abinash Mishra"));
 
 // Search Element into the array
-int[] inArr = { 1, 2, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6 };
-sa.Demo2(inArr, 4);
+//int[] inArr = { 1, 2, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6 };
+//sa.Demo2(inArr, 4);
+
+/*
+    Create an algorithm that deletes the Kth node from the end of the list.
+*/
+
+
+//// Define an array of integers to work on
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//// Use Parallel.ForEach to process each number in the array in parallel
+//Parallel.ForEach(numbers, number =>
+//{
+//    // Do some work on each number
+//    Console.WriteLine("Processing number {0} on thread {1}", number, Task.CurrentId);
+//});
+
+//Console.WriteLine("All numbers processed.");
+//Console.ReadLine();
+
+//---------------------------
+
+var flag = Convert.ToBoolean("False");
+Console.WriteLine("flag value = " + flag);
+
+if (flag) Console.WriteLine("I received flag value as True");
+else Console.WriteLine("Received flag value as False");
+
+//---------------------------
+
+
+
 
